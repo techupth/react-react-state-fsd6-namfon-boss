01f3ff9456a627ff43dc6c612e66375ef06a761d
Namfon <varisa.vb@hotmail.com>
2024-06-05 11:37:58 +0700

    feat(App.jsx): Add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setMessage("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => setMessage("Hi!")}>Hi!</button>
+        <button onClick={() => setMessage("你好!")}>你好!</button>
       </div>
     </div>
   );
