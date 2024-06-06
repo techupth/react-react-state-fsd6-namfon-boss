57e14a7c718af8d928d7aab651895c7bc748925d
paphonwat <paphonwat.w@gmail.com>
2024-06-07 02:25:52 +0700

    feat: sentReact React State Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import "./App.css";
 
+
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message")
+  const handleClick = (text) => {
+    setGreetingMessage(text)
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => {handleClick("สวัสดี")}}>สวัสดี!</button>
+        <button onClick={() => {handleClick("Hi")}}>Hi!</button>
+        <button onClick={() => {handleClick("你好")}}>你好!</button>
       </div>
     </div>
   );
